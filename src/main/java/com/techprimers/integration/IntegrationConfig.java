1ae8f140672c409fa4ac3c2b0291424a9dac5acd
Shrirang Edgaonkar <shrirange@gmail.com>
2021-12-26 22:57:33 +0530

    First cut modifications

--- src/main/java/com/techprimers/integration/IntegrationConfig.java
+++ src/main/java/com/techprimers/integration/IntegrationConfig.java
@@ -1,15 +1,23 @@
 package com.techprimers.integration;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.core.MessageSource;
 import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.dsl.IntegrationFlows;
 import org.springframework.integration.dsl.Pollers;
+import org.springframework.integration.file.DirectoryScanner;
 import org.springframework.integration.file.FileReadingMessageSource;
 import org.springframework.integration.file.FileWritingMessageHandler;
+import org.springframework.integration.file.RecursiveDirectoryScanner;
+import org.springframework.integration.file.filters.AcceptOnceFileListFilter;
+import org.springframework.integration.file.filters.CompositeFileListFilter;
+import org.springframework.integration.file.filters.RegexPatternFileListFilter;
 
 import java.io.File;
+import java.util.Arrays;
 
 @Configuration
 public class IntegrationConfig {
@@ -18,10 +26,10 @@ public class IntegrationConfig {
     private Transformer transformer;
 
     @Bean
-    public IntegrationFlow integrationFlow() {
-        return IntegrationFlows.from(fileReader(),
+    public IntegrationFlow integrationFlow( MessageSource<File> fileReader) {
+        return IntegrationFlows.from(fileReader,
                 spec -> spec.poller(Pollers.fixedDelay(500)))
-                .transform(transformer, "transform")
+                //.transform(transformer, "transform")
                 .handle(fileWriter())
                 .get();
     }
@@ -36,10 +44,22 @@ public class IntegrationConfig {
     }
 
     @Bean
-    public FileReadingMessageSource fileReader() {
+    public FileReadingMessageSource fileReader(DirectoryScanner directoryScanner) {
         FileReadingMessageSource source = new FileReadingMessageSource();
         source.setDirectory(new File("source"));
+        source.setScanner(directoryScanner);
         return source;
     }
 
+    
+    @Bean
+    public DirectoryScanner directoryScanner(@Value("${inbound.filename.regex}") String regex) {
+        DirectoryScanner scanner = new RecursiveDirectoryScanner();
+        CompositeFileListFilter<File> filter = new CompositeFileListFilter<>(
+                Arrays.asList(new AcceptOnceFileListFilter<>(),
+                        new RegexPatternFileListFilter(regex))
+        );
+        scanner.setFilter(filter);
+        return scanner;
+    }
 }
--- src/main/java/com/techprimers/integration/Transformer.java
+++ src/main/java/com/techprimers/integration/Transformer.java
@@ -10,6 +10,8 @@ import java.nio.file.Paths;
 public class Transformer {
 
     public String transform(String filePath) throws IOException {
+    	
+    	System.out.println("filePath :" + Paths.get(filePath).toAbsolutePath());
 
         String content = new String(Files.readAllBytes(
                 Paths.get(filePath)));
